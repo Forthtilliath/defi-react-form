4ead9119f259faaf97915b83f27ef1eacd0532e2
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-07-07 15:30:58 +0200

    date rdv

--- src/components/utils.js
+++ src/components/utils.js
@@ -16,3 +16,25 @@ export const isEmpty = (value) =>
 //     (typeof value === "string" && value.trim().length === 0)
 //   );
 // };
+export const timestampParser = (timestamp) => {
+  let options = {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  return new Date(timestamp).toLocaleDateString("fr-FR", options).toString();
+};
+
+export const generateDate = () => {
+  const now = new Date();
+  const hours = Math.floor(Math.random() * 11 + 8);
+  const minutes = Math.floor(Math.random() * 4) * 15;
+  const day = now.getDate() + Math.floor(Math.random() * 14 + 1);
+
+  return new Date(now.getFullYear(), now.getMonth(), day, hours, minutes, 0);
+};
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -3,12 +3,9 @@ import { NavLink, useHistory } from "react-router-dom";
 import { MDBBtn } from "mdb-react-ui-kit";
 import datasForm from "../../datas/datasForm";
 import { useEffect } from "react";
+import { timestampParser, generateDate } from "../../components/utils";
 
-const Resume = ({
-  verifyForm,
-  formValues,
-  setFormValues,
-}) => {
+const Resume = ({ verifyForm, formValues, setFormValues }) => {
   // const history = useHistory();
   const allIsFine = verifyForm(false);
 
@@ -17,6 +14,7 @@ const Resume = ({
   //     // history.push("/");
   //   }
   // }, [allIsFine, history, props]);
+  const dateRdv = timestampParser(generateDate());
 
   const {
     datasSexe,
@@ -30,24 +28,46 @@ const Resume = ({
     <>
       <div className="container mt-3">
         {allIsFine ? (
-          <div className="row">
-            <div className="col-md-12 text">
-              <div>Récapitulatif des informations :</div>
-              <div className="square border border-light rounded p-3">
-                {datasSexe.find((sexe) => sexe.id === formValues.sexe)?.name},{" "}
-                {formValues.lastname} {formValues.firstname}
-                <br />
-                {formValues.addressStreet}
-                <br />
-                {formValues.addressBp}
-                {formValues.addressCity}
-                <br />
-                {formValues.phoneNumber}
-                {formValues.email}
-                <hr />
+          <>
+            <div className="row">
+              <div className="col-md-12 text">
+                Merci beaucoup pour ces dons que vous allez faire ! Cela va
+                aider un grand nombre de personnes.
               </div>
             </div>
-          </div>
+            <div className="row">
+              <div className="col-md-12 text">
+                Nous sommes fabuleux, nous sommes magnifique, nous avons donc
+                décidé du jour où vous viendriez faire ces dons. Pensez à bien
+                l'enregistrer :
+                <span
+                  style={{
+                    fontWeight: "bold",
+                    display: "block",
+                    marginLeft: "1rem",
+                  }}
+                >
+                  {"⇨"} {dateRdv}
+                </span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12 text">
+                <div>Récapitulatif des informations :</div>
+                <div className="square border border-light rounded p-3">
+                  {datasSexe.find((sexe) => sexe.id === formValues.sexe)?.name},{" "}
+                  {formValues.lastName} {formValues.firstName}
+                  <br />
+                  {formValues.addressStreet}
+                  <br />
+                  {formValues.addressBp} {formValues.addressCity}
+                  <br />
+                  {formValues.phoneNumber} {formValues.email}
+                  <hr />
+                </div>
+              </div>
+            </div>
+          </>
         ) : (
           <div className="bg-danger rounded text-light p-3 mt-5">
             Toutes les informations attendues n'ont pas été reçues.
