a85cca64cec2f20f52f9efb5d3b6a091b01f1424
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-07-07 12:53:43 +0200

    gestion erreur btn radio

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/Home/Home";
 import Formulaire from "./pages/Formulaire/Formulaire";
 import Resume from "./pages/Resume/Resume";
 import { useEffect } from "react";
+import { isEmpty } from "./components/utils";
 
 // https://mdbootstrap.com/docs/standard/forms/input-fields/
 function App() {
@@ -26,7 +27,21 @@ function App() {
     sperm: "",
   });
 
-  const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState({
+    sexe: "",
+    lastName: "",
+    firstName: "",
+    addressStreet: "",
+    addressBp: "",
+    addressCity: "",
+    phoneNumber: "",
+    email: "",
+    kidney: "",
+    lung: "",
+    basic: "",
+    skin: "",
+    sperm: "",
+  });
 
   useEffect(() => {
     // Retire tous les espaces, puis en ajoute un tous les 2 caractères
@@ -36,19 +51,33 @@ function App() {
     setFormValues({ ...formValues, phoneNumber: temp });
   }, [formValues.phoneNumber]);
 
-  const isEmpty = (value) => value === "" || value.length === 0;
+  //const isEmpty = (value) => value === "" || value.length === 0;
+
+  const verifyCoordonnees = () => {
+    const objErrors = {};
+    if (isEmpty(formValues.sexe))
+      objErrors.sexe = "Merci de sélectionner un sexe";
+    if (isEmpty(formValues.lastName))
+      objErrors.lastName = "Merci de saisir votre nom";
+    if (isEmpty(formValues.firstName))
+      objErrors.firstName = "Merci de saisir votre prénom";
+    if (isEmpty(formValues.addressStreet))
+      objErrors.addressStreet = "Merci de saisir votre adresse";
+    if (isEmpty(formValues.addressBp))
+      objErrors.addressBp = "Merci de saisir votre code postal";
+    if (isEmpty(formValues.addressCity))
+      objErrors.addressCity = "Merci de saisir votre ville";
+    if (isEmpty(formValues.phoneNumber))
+      objErrors.phoneNumber = "Merci de saisir votre numéro de téléphone";
+    if (isEmpty(formValues.email))
+      objErrors.email = "Merci de saisir votre email";
+
+    setErrors({ ...objErrors });
+    return objErrors === {};
+  };
 
   const verifyForm = () => {
-    return !(
-      isEmpty(formValues.sexe) ||
-      isEmpty(formValues.lastName) ||
-      isEmpty(formValues.firstName) ||
-      isEmpty(formValues.addressStreet) ||
-      isEmpty(formValues.addressBp) ||
-      isEmpty(formValues.addressCity) ||
-      isEmpty(formValues.phoneNumber) ||
-      isEmpty(formValues.email)
-    );
+    return verifyCoordonnees();
   };
 
   return (
@@ -62,6 +91,8 @@ function App() {
               verifyForm={verifyForm}
               formValues={formValues}
               setFormValues={setFormValues}
+              errors={errors}
+              setErrors={setErrors}
             />
           </Route>
           <Route path="/resume" exact>
